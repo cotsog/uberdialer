ef8d6b597e5a49f6f7e53354b7104eb5451d8e27
Khanjan Kukaidya <kkukadiya@synoptek.com>
2018-10-25 15:02:35 +0530

    Changes in config.php file.

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -1,5 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://uberdialer-latest.local/';
+$config['base_url'] = 'http://uberdialer-indusa.local/';
 
 /*
 |--------------------------------------------------------------------------
